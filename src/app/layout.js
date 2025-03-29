ff1b188dd64e3045fde7b221aa8d66c28d185532
rojass2001 <rojascherian@gmail.com>
2025-03-30 00:01:55 +0530

    third commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter} from "next/font/google";
 import "./globals.css";
-import Navbar from "./navbar/Navbar";
+import Navbar from "./Navbar";
 import Contactfooter from "./Contactfooter";
 import Footer from "./Footer";
 
