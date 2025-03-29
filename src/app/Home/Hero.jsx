9f3b2592ed6244a86235518e1f97c066e03aa2f4
rojass2001 <rojascherian@gmail.com>
2025-03-29 23:22:06 +0530

    third commit

--- src/app/Home/Hero.jsx
+++ src/app/Home/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
     <>
  <div className='w-full mt-[60px] '>
    <div className='w-full   md:px-11 relative place-items-center 
-      place-content-center bg-cover  bg-no-repeat   min-h-screen px-6'style={{backgroundImage:`url(${'/hero.jpg'})`}}>   
+      place-content-center bg-cover  bg-no-repeat   min-h-screen px-6'style={{backgroundImage:`url(${'./hero.jpg'})`}}>   
       <h1 className=' text-3xl md:text-6xl text-center font-bold  text-white'>Search your Next Homes</h1>
      <p className='font-semibold  text-center text-white mt-3 text-xl inline-block'>Find new & featured property located in your <span className='text-center'>local city.</span></p>
      <div className='w-full mt-16 bg-white
