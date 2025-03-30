73a80375bdf4eb4c360d8aef20146b42d47166ea
rojass2001 <rojascherian@gmail.com>
2025-03-30 12:24:57 +0530

    third commit

--- src/app/Contactfooter.jsx
+++ src/app/Contactfooter.jsx
@@ -4,7 +4,7 @@ return (
   <footer className='w-full  flex items-center justify-between py-8 text-white
        bg-green-500 md:px-3  px-1 lg:px-20 '>
       <div className=''>
-         <h1 className='font-bold text-2xl md:text-[35px] lg:text-[43px] '>Do You Have Questions?</h1>
+         <h1 className='font-bold text-2xl md:text-[35px] lg:text-[43px] '>Do You Have Questions</h1>
          <p className='text-xl '>We will help you to grow your career and growth.</p>
       </div>
         <div className='bg-white flex items-center  font-semi-bold text-[20px] text-green-500 text-center 
