66dbb68bc3a7be194706340512abc83c3703a1d6
rojass2001 <rojascherian@gmail.com>
2025-05-08 21:46:23 +0530

    second commit

--- src/app/customhooks/useauth.js
+++ src/app/customhooks/useauth.js
@@ -47,9 +47,9 @@ export default function useAuthentication(email, password) {
     try {
       await signInWithEmailAndPassword(auth, email, password);
       Cookies.set('login', JSON.stringify(true), { expires: 7 }); // Set login state in cookies
+      await setcurrentactivetab(0) // Reset active tab to 0 after login
       toast.success("Login successful");
-      router.push("/"); // Redirect to home page after sucessfully login
-      setcurrentactivetab(0) // Reset active tab to 0 after login
+      router.push("/") // Redirect to home page after sucessfully login
 
     } catch (error) {
       toast.error("Invalid username or password");
