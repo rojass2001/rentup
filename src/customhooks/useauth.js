c39d0756f292c008f4408e26d6c625bd59cd728f
rojass2001 <rojascherian@gmail.com>
2025-05-09 13:52:34 +0530

    second commit

--- src/customhooks/useauth.js
+++ src/customhooks/useauth.js
@@ -47,8 +47,8 @@ export default function useAuthentication(email, password) {
     try {
       await signInWithEmailAndPassword(auth, email, password);
       Cookies.set('login', JSON.stringify(true), { expires: 7 }); // Set login state in cookies
-      router.push("/"); // Redirect to home page after sucessfully login
       toast.success("Login successful");
+      router.push("/");//redirect to home page after login
       setcurrentactivetab(0); // Reset active tab to 0 after login
 
     } catch (error) {
